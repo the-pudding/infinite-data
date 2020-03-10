bddd9be49bc6758263685628208df703e5687fca
russellgoldenberg <russellgoldenberg@gmail.com>
2020-03-10 09:09:44 -0400

    hundred again

--- index.js
+++ index.js
@@ -63,7 +63,10 @@ function generateAttempts({ range, sequence, iterations }) {
 
   if (DEV) console.time("makeAttempt");
 
-  while (i < 1000000000) {
+  iterations = 100000000;
+  const mod = iterations / 10;
+  while (i < iterations) {
+    if (i % mod === 0) console.log(i / iterations);
     output[i % RECENT] = makeAttempt();
     if (done) break;
     i += 1;
